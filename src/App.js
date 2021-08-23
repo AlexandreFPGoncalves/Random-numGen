ad8472ebebcd9cab1451766fa862015e82114627
Alexandre Gonçalves <alexandre.goncalves@itsector.pt>
2021-08-23 13:25:19 +0100

    Test GitHub - Updated src/App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { RandomNumber } from "./components/RandomNumber";
+
+// : ) !!!!
+function App() {
+  return <RandomNumber />;
+}
+
+export default App;
