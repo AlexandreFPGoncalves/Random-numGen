b6cd1687da04f003b23ddd92789bf03e96bc14a6
Alexandre Gonçalves <88319848+AlexandreFPGoncalves@users.noreply.github.com>
2022-02-17 07:24:13 +0000

    Small changes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Project Hosted In the following Link
+
+[Random Number generator](https://AlexandreFPGoncalves.github.io/Random-Number-Generator)
+
 ## Install dependencies
 
 In your terminal after you clone your project down, remember to run either `yarn` or `npm install` to build all the dependencies in the project.
--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import { RandomNumberScreen } from './pages/randomNumber/randomNumber';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<RandomNumberScreen />} />
-				<Route path="*" element={<div>ErrorScreen</div>} />
-			</Routes>
-		</BrowserRouter>
-	);
+	return <RandomNumberScreen />;
 }
 
 export default App;
